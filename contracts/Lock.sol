2680a99e73fa5d9bee23abbf04f25c6107589308
SerhatCelikk <90610606+SerhatCelikk@users.noreply.github.com>
2022-07-18 12:58:14 +0300

    ,

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -2,33 +2,43 @@
 pragma solidity ^0.8.2;
 
 import "./Token.sol";
+import "hardhat/console.sol";
 
 contract Lock {
-   SRTToken Token;
-   uint256 public lockerCount;
-   uint256 public totalLocked;
-   mapping(address => uint256) public lockers;
-   constructor(address tokenAddress){
-    Token = SRTToken(tokenAddress);
-   }
-   function lockTokens(uint256 amount) external {
-    require(amount>0,"Token amount must be bigger than 0.");
-       //   require(Token.balanceOf(msg.sender)>=amount,"Insufficent funds");
-      //   require(Token.allowance(msg.sender, address(this))>=amount,"Insufficent allowance");
-    if(!(lockers[msg.sender] > 0))  lockerCount++;
-    totalLocked+=amount;
-    lockers[msg.sender]+=amount;
+    SRTToken Token;
+    uint256 public lockerCount;
+    uint256 public totalLocked;
+    mapping(address => uint256) public lockers;
+    mapping(address => uint256) public deadLine;
 
-    bool ok=Token.transferFrom(msg.sender, address(this), amount);
-    require(ok,"Transfer Failed");
-   }
+    constructor(address tokenAddress) {
+        Token = SRTToken(tokenAddress);
+    }
 
-   function withdrawTokens() external {
-    require(lockers[msg.sender]>0,"Not enough token.");
-    uint256 amount = lockers[msg.sender];
-    delete(lockers[msg.sender]);
-    totalLocked -=amount;
-    lockerCount--;
-    require(Token.transfer(msg.sender, amount),"Transfer Failed");
-   }
+    function lockTokens(uint256 amount, uint256 time) external {
+        require(amount > 0, "Token amount must be bigger than 0.");
+        require(time > 0, "Token amount must be bigger than 0.");
+
+        //   require(Token.balanceOf(msg.sender)>=amount,"Insufficent funds");
+        //   require(Token.allowance(msg.sender, address(this))>=amount,"Insufficent allowance");
+        if (!(lockers[msg.sender] > 0)) lockerCount++;
+        totalLocked += amount;
+        lockers[msg.sender] += amount;
+        deadLine[msg.sender]= block.timestamp + time;
+
+        bool ok = Token.transferFrom(msg.sender, address(this), amount);
+        require(ok, "Transfer Failed");
+    }
+
+    function withdrawTokens() external {
+        require(lockers[msg.sender] > 0, "Not enough token.");
+        require(block.timestamp>= deadLine[msg.sender],"Deadline is not over");
+        console.log("withdraw function running");
+        uint256 amount = lockers[msg.sender];
+        delete (lockers[msg.sender]);
+        delete(deadLine[msg.sender]);
+        totalLocked -= amount;
+        lockerCount--;
+        require(Token.transfer(msg.sender, amount), "Transfer Failed");
+    }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,14 @@ const PRIVATE_KEY = "9ea5c3762c236ab586e48f94a7003c05cd809c1d383ee8ee8a30562eb05
 module.exports = {
     solidity: "0.8.2",
     networks: {
+
+      hardhat: {
+        forking: {
+          url: "https://eth-rinkeby.alchemyapi.io/v2/sf7Mr0i_qcauE2bqJWAuR9syrrJ8y8vA",
+          // blockNumber: 1234567
+        }
+      },
+
       mainnet: {
         url: `https://api.avax.network/ext/bc/C/rpc`,
           accounts: [`${PRIVATE_KEY}`]
